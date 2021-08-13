eaa71be24641e04e60583d3736f4ee85b25ea2cc
Nikoloz Kipiani <77412876+KipianiNikoloz@users.noreply.github.com>
2021-08-13 15:13:16 +0400

    first commit

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+  ) {}
+
+  private url: string = 'https://recipeapplication-f5be4-default-rtdb.europe-west1.firebasedatabase.app/recipies.json';
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        this.url,
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        this.url
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'key=AIzaSyBolIdKgBBZvt4YWWzx_NoxVv_zwMtP5xU'
+};
